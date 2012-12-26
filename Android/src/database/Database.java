2e3779fe5fd7c3f8ce5ad9a552f1c0ecb92fdfa3
leeken <kennyl606@gmail.com>
2012-12-26 00:01:55 -0500

    Added AndroidDatabase, modified Model

---
+++ Android/src/database/Database.java
@@ -0,0 +1,27 @@
+package database;
+
+
+/* constructor should init all things needed to store every
+ * type of data
+ * 
+ * 
+ * */
+public interface Database {
+
+	// Stores the second count. Saves the song, 
+	// the second of the song started and stopped playing at
+			// If whole song numbers would be 0 and song.length
+	// stores the time this insert was recorded
+	// end_sec - start_sec = number of seconds played since last insert
+	
+	/*	---Text file example---
+	 * 	C:/.../Settle Down.mp3
+	 * 	0	17
+	 * 	Tue Sep 25 00:11:34 2012
+	 * 	|~|
+	 */
+
+	
+	//void insert_sec_count(String filename, long start_sec, long end_sec);
+	void insert_sec_count(String filename, long start_sec, long end_sec);
+}
